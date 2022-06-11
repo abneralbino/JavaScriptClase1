84ca6b08dc8eda8167fe14b18515a168322c1ae6
abner.albino <abner.albino@gmail.com>
2022-06-11 14:46:58 -0300

    Starting with JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hola a todos!");
+/* //recommended: use let instead of var
+
+let numero = 1234; //declared and defined
+const NUMERO = 1234;
+
+
+
+//not recommended
+var nombre = "Abner";
+
+console.log(typeof(nombre)); //To know it it is a number or a string, strings use "" and number does not
+ */
+
+/* let numero1 = 4;
+let numero2 = 5;
+let resultMult = numero1 * numero2;
+let resultAdd = numero1 + numero2;
+let resultDiv = numero1 / numero2;
+let resultMin = numero1 - numero2;
+
+console.log(resultAdd);
+console.log(resultMult);
+console.log(resultDiv);
+console.log(resultMin); */
+
+
+
+/* let text1 = "Andres";
+let text2 = "Marian";
+
+console.log(text1 + text2); //namestogether
+console.log(text1 + " " + text2); //names with a space */
